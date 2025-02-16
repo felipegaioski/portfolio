64ca980a16eb375a8fdc859ce61ca4c4992d3469
felipegaioski <felipegaioski@gmail.com>
2025-02-16 20:14:34 -0300

    fix images

--- src/components/gear.tsx
+++ src/components/gear.tsx
@@ -11,32 +11,32 @@ const gear = [
             {
                 name: "Canon EOS Rebel SL2",
                 description: "It's been my companion since 2020, and I just love it. As many would say, the best camera is the one you have.",
-                image: "/images/sl2.png",
+                image: "https://i.postimg.cc/pLCqjDxD/sl2.png",
             },
             {
                 name: "Praktica MTL3",
                 description: "My father bought this camera back in 1982 because he wasn't satisfied with his Yashica point-and-shoot. I can say it was the best decision he could've done. He used it until the early 2000's. In 2023 I picked it up from the shelf to begin in film photography.",
-                image: "/images/praktica.png",
+                image: "https://i.postimg.cc/J4bT1f2Y/praktica.png",
             },
             {
                 name: "Canon EOS 5000",
                 description: "Kindly lent to me. It's a nice modern film camera. Not so many controls available, but does a great job!",
-                image: "/images/eos5000.png",
+                image: "https://i.postimg.cc/fRNrmzdh/eos5000.png",
             },
             {
                 name: "Olympus Trip 35",
                 description: "The classic of classics.",
-                image: "/images/trip35.png",
+                image: "https://i.postimg.cc/Lssv3n6G/trip35.png",
             },
             {
                 name: "Olympus Trip MD3",
                 description: "Lent to me as well. Had to fix a few things, but works nicely now. A simple, but fun to use point-and-shoot.",
-                image: "/images/md3.png",
+                image: "https://i.postimg.cc/LXnC2hcj/md3.pn",
             },
             {
                 name: "Kapsa Pinta Vermelha",
                 description: "Translates to 'Kapsa Red Dot'. This is a classic brazilian brownie waist level camera from the 1950's. Kindly gifted to me by my grandmother.",
-                image: "/images/kapsa.png",
+                image: "https://i.postimg.cc/0QFtQPvF/kapsa.png",
             },
         ],
     },
@@ -46,32 +46,32 @@ const gear = [
             {
                 name: "Canon EF-S 18-55mm f/4-5.6 IS STM",
                 description: "Yep. A kit lens. A lens that I learned to love. It's nowhere close to a high-end lens, but has made me grow as a photographer. If you don't like yours, throw a mist filter on it. Trust me.",
-                image: "/images/1855.png",
+                image: "https://i.postimg.cc/q7BDD1Qb/1855.png",
             },
             {
                 name: "Canon 50mm f/1.8 EF II",
                 description: "The nifty-fifty.",
-                image: "/images/50.png",
+                image: "https://i.postimg.cc/dtdfhkLB/50.png",
             },
             {
                 name: "Vivitar 28mm f/2.5 Auto Wide Angle",
                 description: "This M42 lens surprised me for how good it is for the price (it's ridiculously cheap). Great colors, great bokeh, great image quality. A nice piece of glass.",
-                image: "/images/vivitar.png",
+                image: "https://i.postimg.cc/jqB9qQd2/vivitar.png",
             },
             {
                 name: "Takumar 100mm f/4 Super-Multi-Coated Macro",
                 description: "Another cheap M42 Lens that really impressed me. Super sharp for a vintage lens.",
-                image: "/images/takumar.png",
+                image: "https://i.postimg.cc/g2mgrM9z/takumar.png",
             },
             {
                 name: "Pentacon 50mm f/1.8",
                 description: "This is the kit lens for the Praktica MTL3. Incredible colors and bokeh.",
-                image: "/images/pentacon.png",
+                image: "https://i.postimg.cc/dQZHfkmY/pentacon.png",
             },
             {
                 name: "Sigma 70-300mm f/4-5.6 DG Macro",
                 description: "Nice long range lens with incredible macro capabilities.",
-                image: "/images/sigma.png",
+                image: "https://i.postimg.cc/d3C5Zpt1/sigma.png",
             },
         ],
     }
